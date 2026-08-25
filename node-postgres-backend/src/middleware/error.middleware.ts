943cecd3171cef8b616e429e329a942c0f7751bf
Abeel mazhar <abeelmazhar4@gmail.com>
2026-08-25 14:13:22 +0500

    Add specific error handling for database errors in middleware. Handle unique constraint violations and invalid ID errors with appropriate HTTP status codes and messages.

--- node-postgres-backend/src/middleware/error.middleware.ts
+++ node-postgres-backend/src/middleware/error.middleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 
+import { DatabaseError } from "pg";
 import { AppError } from "../errors/app.error.js";
 
 export const errorHandler = (
@@ -13,6 +14,22 @@ export const errorHandler = (
   if (error instanceof AppError) {
     res.status(error.statusCode).json({
       message: error.message,
+    });
+
+    return;
+  }
+
+  if (error instanceof DatabaseError && error.code === "23505") {
+    res.status(409).json({
+      message: "Email already exists",
+    });
+
+    return;
+  }
+
+  if (error instanceof DatabaseError && error.code === "22P02") {
+    res.status(400).json({
+      message: "Invalid ID",
     });
 
     return;
