7a2d590919e930777c0fba8b910287316469d9fc
Abeel mazhar <abeelmazhar4@gmail.com>
2026-09-02 10:23:27 +0500

    Add CORS and Helmet middleware for enhanced security and configure express.json limit. Update package dependencies in package.json and package-lock.json.

--- node-postgres-backend/src/server.ts
+++ node-postgres-backend/src/server.ts
@@ -7,11 +7,23 @@ import { authenticate } from "./middleware/auth.middleware.js";
 import { getCurrentUser } from "./controllers/user.controller.js";
 import { authorize } from "./middleware/authorize.middleware.js";
 import adminRoutes from "./routes/admin.routes.js";
+import helmet from "helmet";
+import cors from "cors";
 const app = express();
 
 const PORT = Number(process.env.PORT) || 3000;
 
-app.use(express.json());
+app.use(helmet());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
+app.use(
+  express.json({
+    limit: "100kb",
+  }),
+);
 
 app.use("/users", userRoutes);
 app.use("/auth", authRoutes);
