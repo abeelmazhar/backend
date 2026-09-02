325e55ef6daeb600985ee1297a53e029b79d73ac
Abeel mazhar <abeelmazhar4@gmail.com>
2026-09-02 11:15:26 +0500

    Refactor file upload middleware to use UUIDs for unique filenames and improve file type validation logic. Add a new PNG file to the uploads directory.

--- node-postgres-backend/src/middleware/upload.middleware.ts
+++ node-postgres-backend/src/middleware/upload.middleware.ts
@@ -1,27 +1,33 @@
 import multer from "multer";
-
-const allowedTypes = ["image/jpeg", "image/png", "image/webp"];
+import crypto from "crypto";
+import path from "path";
 
 const storage = multer.diskStorage({
   destination: "./uploads",
 
   filename: (req, file, cb) => {
-    const uniqueName = `${Date.now()}-${file.originalname}`;
+    const extension = path.extname(file.originalname);
 
-    cb(null, uniqueName);
+    const filename = `${crypto.randomUUID()}${extension}`;
+
+    cb(null, filename);
   },
 });
 
+const allowedTypes = ["image/jpeg", "image/png", "image/webp"];
+
 export const upload = multer({
   storage,
+
   limits: {
     fileSize: 5 * 1024 * 1024,
   },
+
   fileFilter: (req, file, cb) => {
-    if (allowedTypes.includes(file.mimetype)) {
-      cb(null, true);
-    } else {
-      cb(new Error("Only JPEG, PNG and WebP images are allowed"));
+    if (!allowedTypes.includes(file.mimetype)) {
+      return cb(new Error("Only JPEG, PNG and WebP images are allowed"));
     }
+
+    cb(null, true);
   },
 });
